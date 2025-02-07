64f8b0c1bb54725dbfc771256e1a6fc55d9b2d45
Sebastian Araya <s-araya0021@gmail.com>
2025-02-06 22:15:30 -0300

    arreglo-config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://s-araya0021.github.io',
-  base: '/astro-portfolio-SA',
+  site: 'https://S-Araya0021.github.io',
+  base: 'astro-portfolio-SA',
 });
